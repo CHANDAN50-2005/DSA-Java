aecd3e1a040ee9875f2cae20b9c4fd6345e73c6b
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-19 23:28:24 +0530

    Time: 1 ms (99.97%), Space: 47.1 MB (13.71%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        for(int i =0; i<n; i++){
-            int num = nums[i];
-            int count = 0;
-            for(int j=0; j<n; j++){
-                if(nums[j] == num){
-                    count++;
-                }
-            }
-            if(count == 1) return num;
+        int ans = 0;
+        for(int n : nums){
+            //ans = ans ^ n;
+            ans^= n;  // xor which cancel out the same num
+                      // a^a = 0, 2^3^3 = 2
         }
-        return 0;
+        return ans;
     }
 }
