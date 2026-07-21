acd6ee3c29d0cc9232a5b2be40b52fb443af51c9
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-22 00:12:04 +0530

    Time: 1 ms (99.89%), Space: 55.6 MB (59.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,13 +2,13 @@ class Solution {
     public int majorityElement(int[] nums) {
         int ele = 0;
         int count = 0;
+        // By using Moore's voting algorithm
 
-        for(int num : nums){
-            if(count == 0) {
-                ele = num;
-            }
-            if(num == ele) count++;
-            else count --;    
+        for(int num : nums){  // iter throuh arr
+            if(count == 0) ele = num;   // if there is no value in element add the num 
+                                       // add fisrt itr num
+            if(num == ele) count++; // num and ele is same so increment count
+            else count --;         // if no match decrement the code 
         }
         return ele;
     }
