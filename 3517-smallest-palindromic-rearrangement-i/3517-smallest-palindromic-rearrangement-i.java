c243c2964347b21df9e866c2f493a720fb6e1ce5
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-28 22:48:14 +0530

    Time: 36 ms (25.3%), Space: 47.9 MB (88.35%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -14,27 +14,20 @@ class Solution {
         // Build left half and find middle character
         for (int i = 0; i < 26; i++) {
 
-            // Add half of each character to the left half
+            // Add half of the occurrences to the left half
             for (int j = 0; j < freq[i] / 2; j++) {
                 left.append((char) (i + 'a'));
             }
 
-            // Store the odd frequency character as the middle
+            // Odd frequency character becomes the middle
             if (freq[i] % 2 == 1) {
                 middle = String.valueOf((char) (i + 'a'));
             }
         }
 
-        // Build the final answer
-        StringBuilder ans = new StringBuilder(left);
+        // Right half is reverse of left half
+        String right = new StringBuilder(left).reverse().toString();
 
-        ans.append(middle);
-
-        // Mirror the left half
-        for (int i = left.length() - 1; i >= 0; i--) {
-            ans.append(left.charAt(i));
-        }
-
-        return ans.toString();
+        return left.toString() + middle + right;
     }
 }
