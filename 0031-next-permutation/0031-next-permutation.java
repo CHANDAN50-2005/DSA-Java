e429e23d468b2936b3f32ab8d9aee3f7462f4e64
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-29 00:01:08 +0530

    Time: 0 ms (100%), Space: 44.4 MB (84.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,56 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int pivot =-1;
-        for(int i = nums.length-2; i>=0; i--){
-            if(nums[i] < nums[i+1]){
+
+        // Find the pivot (first decreasing element from the right) num < num+1...
+        int pivot = -1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 pivot = i;
                 break;
             }
         }
-        if(pivot == -1){
+
+        // No pivot => Largest permutation, so reverse the whole array
+        if (pivot == -1) {
+
             int st = 0;
-            int end = nums.length-1;
-            while(st <= end){
-                swap(nums,st,end);
+            int end = nums.length - 1;
+
+            while (st < end) {
+                swap(nums, st, end);
                 st++;
                 end--;
             }
-        }
-        else{
+        } else {
+
+            // Find the next greater element from the right
             int second = 0;
-            for(int j = nums.length-1; j > pivot; j--){
-                if(nums[j] > nums[pivot]){
+
+            for (int j = nums.length - 1; j > pivot; j--) {
+                if (nums[j] > nums[pivot]) {
                     second = j;
                     break;
                 }
             }
-            swap(nums,pivot,second);
-            int m = pivot+1;
-            int n = nums.length-1;
-            while(m < n){
-                swap(nums,m,n);
-                m++;
-                n--;
-            }
 
+            // Swap pivot with next greater element
+            swap(nums, pivot, second);
+
+            // Reverse the suffix to get the smallest order
+            int st = pivot + 1;
+            int end = nums.length - 1;
+
+            while (st < end) {
+                swap(nums, st, end);
+                st++;
+                end--;
+            }
         }
-                
     }
-    void swap(int [] nums, int a, int b){
+
+    // Utility method to swap two elements
+    void swap(int[] nums, int a, int b) {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
