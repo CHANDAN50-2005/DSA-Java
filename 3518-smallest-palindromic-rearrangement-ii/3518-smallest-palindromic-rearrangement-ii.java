15c58e06604b92c4f9edaa111713b58c63607bc3
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-29 23:25:29 +0530

    Time: 17 ms (83.87%), Space: 47.1 MB (74.19%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -1,108 +1,73 @@
 class Solution {
-
-    static final long LIMIT = 1_000_000L;
-
     public String smallestPalindrome(String s, int k) {
-
         int[] freq = new int[26];
-
-        for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
         }
-
+        
         int[] half = new int[26];
-        String mid = "";
-
-        int halfLen = 0;
-
-        for (int i = 0; i < 26; i++) {
-            half[i] = freq[i] / 2;
-            halfLen += half[i];
-
-            if ((freq[i] & 1) == 1) {
-                mid = String.valueOf((char) ('a' + i));
+        StringBuilder mid = new StringBuilder();
+        int m = 0;
+        
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] % 2 != 0) {
+                mid.append((char) (i + 'a'));
             }
+            half[i] = freq[i] / 2;
+            m += half[i];
         }
-
-        if (countWays(half, halfLen) < k) {
+        
+        if (getWays(half, k) < k) {
             return "";
         }
-
-        StringBuilder left = new StringBuilder();
-
-        for (int pos = 0; pos < halfLen; pos++) {
-
-            for (int c = 0; c < 26; c++) {
-
-                if (half[c] == 0)
-                    continue;
-
-                half[c]--;
-
-                long ways = countWays(half, halfLen - pos - 1);
-
-                if (ways >= k) {
-
-                    left.append((char) ('a' + c));
-                    break;
-
-                } else {
-
-                    k -= ways;
-                    half[c]++;
-
+        
+        StringBuilder firstHalf = new StringBuilder();
+        for (int i = 0; i < m; ++i) {
+            for (int c = 0; c < 26; ++c) {
+                if (half[c] > 0) {
+                    half[c]--;
+                    long ways = getWays(half, k);
+                    
+                    if (ways >= k) {
+                        firstHalf.append((char) (c + 'a'));
+                        break;
+                    } else {
+                        k -= ways;
+                        half[c]++;
+                    }
                 }
             }
         }
-
-        StringBuilder ans = new StringBuilder();
-
-        ans.append(left);
-        ans.append(mid);
-        ans.append(new StringBuilder(left).reverse());
-
-        return ans.toString();
+        
+        StringBuilder res = new StringBuilder(firstHalf);
+        res.append(mid);
+        res.append(firstHalf.reverse());
+        return res.toString();
     }
-
-    private long countWays(int[] cnt, int total) {
-
-        long res = 1;
-
-        int remain = total;
-
-        for (int x : cnt) {
-
-            if (x == 0)
-                continue;
-
-            res *= combination(remain, x);
-
-            if (res > LIMIT)
-                return LIMIT;
-
-            remain -= x;
+    
+    private long getWays(int[] f, long targetK) {
+        long ways = 1;
+        int currLen = 0;
+        for (int count : f) {
+            if (count > 0) {
+                currLen += count;
+                long n = currLen;
+                long r = count;
+                
+                if (r > n - r) r = n - r;
+                long curNCr = 1;
+                
+                for (int i = 1; i <= r; ++i) {
+                    curNCr = curNCr * (n - i + 1) / i;
+                    if (curNCr > targetK) {
+                        curNCr = targetK + 1;
+                        break;
+                    }
+                }
+                ways *= curNCr;
+                if (ways > targetK) return targetK + 1;
+            }
         }
-
-        return res;
-    }
-
-    private long combination(int n, int r) {
-
-        if (r > n)
-            return 0;
-
-        r = Math.min(r, n - r);
-
-        long ans = 1;
-
-        for (int i = 1; i <= r; i++) {
-
-            ans = ans * (n - r + i) / i;
-
-            if (ans > LIMIT)
-                return LIMIT;
-        }
-
-        return ans;
+        return ways;
     }
 }
