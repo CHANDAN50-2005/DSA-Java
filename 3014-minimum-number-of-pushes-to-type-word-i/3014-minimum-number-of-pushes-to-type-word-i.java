0c9b605609236c5d3a2c8e8faf1311098fc08533
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-30 22:14:58 +0530

    Time: 0 ms (100%), Space: 43.4 MB (16.51%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,9 +1,14 @@
 class Solution {
     public int minimumPushes(String word) {
+        // we can rearrage dailer pad anyway
+        // 2-9 so the total keys are 8
+        // place elements one by one on key
+        // if 1-7 / 8 +1  means 1 push
+        // if 8/8 +1 means 2 push
         int ans = 0;
 
         for(int i=0; i<word.length(); i++){
-            ans += (i/8)+1;
+            ans += (i/8)+1; // till repeatation of ele the push will be 1 in add
         }
         return ans;
     }
