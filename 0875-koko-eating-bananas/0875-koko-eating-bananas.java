c148ed15e652fdc257b4402b027f76f27026e89d
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-31 00:08:03 +0530

    Time: 8 ms (67.84%), Space: 47.4 MB (98.03%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int st= 1;
+        int end = max(piles);
+        int ans = end;
+
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            long totalHrs = total(piles, mid);
+
+            if(totalHrs <= h){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                st = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    long total(int[] piles, int mid){
+        long hrs = 0;
+
+        for(int num : piles){
+            hrs += (num + mid-1L)/mid;
+        }
+        return hrs;
+    }
+
+    int max(int[] piles){
+        int maxi = piles[0];
+        for(int num: piles){
+            if(num > maxi){
+                maxi = num;
+            }
+        }
+        return maxi;
+    }
+}
