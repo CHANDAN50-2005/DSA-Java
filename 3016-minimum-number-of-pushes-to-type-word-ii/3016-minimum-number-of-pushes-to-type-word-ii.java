a418f4214004a5d9ed58489a8efae1abcac5d2ea
Chandan Gouda <mrchandangouda50@gmail.com>
2026-07-31 23:28:44 +0530

    Time: 11 ms (34.1%), Space: 48.3 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for(char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        } 
+
+        Arrays.sort(freq);
+
+        int index = 0;
+        int ans = 0;
+        for(int i=25; i>=0; i--){
+            if(freq[i] == 0) continue;
+
+            int pushes = (index/8)+1;
+            ans += freq[i] * pushes;
+
+            index++;
+        }
+        return ans;
+    }
+}
